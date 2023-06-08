42cc9510ff986187c380d725fbe7ff4682b68d93
rifki alamsyah <m.rifki_si19@nusaputra.ac.id>
2023-06-08 23:29:52 +0700

    ADD Slicing Category by genres (Not workit)

--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -5,6 +5,7 @@ import { MoviesList, MoviesSearch } from "../../components";
 
 function Movies() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("all");
   const dispatch = useDispatch();
   const searchResults = useSelector((state) => state.movies.searchResults);
   const loading = useSelector((state) => state.movies.loading);
@@ -13,14 +14,17 @@ function Movies() {
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
-
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+    dispatch(searchMovies("", category));
+  };
+  
   useEffect(() => {
-    dispatch(searchMovies(searchQuery));
-  }, [dispatch, searchQuery]);
+    dispatch(searchMovies(searchQuery, selectedCategory));
+  }, [dispatch, searchQuery, selectedCategory]);
 
   return (
     <div className="moviesPage container">
-      {console.log(searchResults)}
       <h1 className="text-white">Popular Movies</h1>
 
       <div className="row mb-4">
@@ -35,6 +39,59 @@ function Movies() {
         </div>
       </div>
 
+      <div className="navigation-kategori mb-5">
+        <ul className="nav justify-content-center">
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedCategory === "all" ? "active text-white" : "text-white"
+              }`}
+              onClick={() => handleCategoryClick("all")}
+              href="#"
+            >
+              All Movies
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedCategory === "Horror" ? "active text-white" : "text-white"
+              }`}
+              onClick={() => handleCategoryClick("Horror")}
+              href="#"
+            >
+              Horror
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedCategory === "Adventure"
+                  ? "active text-white"
+                  : "text-white"
+              }`}
+              onClick={() => handleCategoryClick("Adventure")}
+              href="#"
+            >
+              Adventure
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={`nav-link ${
+                selectedCategory === "Fantasy"
+                  ? "active text-white"
+                  : "text-white"
+              }`}
+              onClick={() => handleCategoryClick("Fantasy")}
+              href="#"
+            >
+              Fantasy
+            </a>
+          </li>
+        </ul>
+      </div>
+      {console.log(selectedCategory)}
       {loading && <div>Loading...</div>}
       {error && <div>{error}</div>}
       {!loading && !error && (
