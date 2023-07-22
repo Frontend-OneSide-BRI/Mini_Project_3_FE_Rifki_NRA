b13b1ed23806bdef1b829a7074f459eda0c14cab
rifki alamsyah <m.rifki_si19@nusaputra.ac.id>
2023-07-22 16:44:42 +0700

    footer fixed

--- src/pages/Movies/detail.jsx
+++ src/pages/Movies/detail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchMovieDetail } from "../../services/movies/moviesApi";
 import { useParams } from "react-router-dom";
+import { Footer } from "../../components";
 
 const MovieDetail = () => {
   const { movieId } = useParams();
@@ -24,68 +25,64 @@ const MovieDetail = () => {
 
   return (
     <>
-      {movie && (
-        <div className="detail">
-          <div className="detail-img">
-            <img
-              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-              alt=""
-            />
-          </div>
-          <div className="detail-content container pt-5">
-            <div class="row">
-              <div class="col-lg-6 mb-3">
-                <div class="p-5 rounded border border-success">
-                  <center>
-                    <img
-                      src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
-                      alt=""
-                      class="imgmobile"
-                    />
-                  </center>
-
-                  <i>
-                    <h1 class="display-4 text-center fw-bold">{movie.title}</h1>
-                  </i>
-                  <div v-if="movie.summary !== null">
-                    <p class="lead">{movie.overview}</p>
-                  </div>
-                  <hr class="my-4" />
-                  <ul class="list-group fs-5 mb-3">
-                    <li className="list-group-item">
-                      Genres:{" "}
-                      {movie.genres.map((genre) => genre.name).join(", ")}
-                    </li>
-
-                    <li class="list-group-item">
-                      Language : {movie.original_language}
-                    </li>
-                    <li class="list-group-item">
-                      Realise date : {movie.release_date}
-                    </li>
-                    <li class="list-group-item">
-                      Rating : {movie.vote_average}
-                    </li>
-                  </ul>
-                  <p class="lead">
-                    <a href="/" class="btn btn-darkpurple btn-md">
-                      Back To List Movie
-                    </a>
-                  </p>
-                </div>
-              </div>
-              <div class="col-lg-6">
-                <div class="thumb border border-success imgdesktop fade-in-down">
+      <div className="detail">
+        <div className="detail-img">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt=""
+          />
+        </div>
+        <div className="detail-content container pt-5">
+          <div class="row">
+            <div class="col-lg-6 mb-3">
+              <div class="p-5 rounded border-purple">
+                <center>
                   <img
-                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
                     alt=""
+                    class="imgmobile"
                   />
+                </center>
+
+                <i>
+                  <h1 class="display-4 text-center fw-bold">{movie.title}</h1>
+                </i>
+                <div v-if="movie.summary !== null">
+                  <p class="lead">{movie.overview}</p>
                 </div>
+                <hr class="my-4" />
+                <ul class="list-group fs-5 mb-3">
+                  <li className="list-group-item">
+                    Genres: {movie.genres.map((genre) => genre.name).join(", ")}
+                  </li>
+
+                  <li class="list-group-item">
+                    Language : {movie.original_language}
+                  </li>
+                  <li class="list-group-item">
+                    Realise date : {movie.release_date}
+                  </li>
+                  <li class="list-group-item">Rating : {movie.vote_average}</li>
+                </ul>
+                <p class="lead">
+                  <a href="/" class="btn btn-darkpurple btn-md">
+                    Back To List Movie
+                  </a>
+                </p>
               </div>
             </div>
+            <div class="col-lg-5 offset-1">
+              <div class="thumb border-purple imgdesktop fade-in-down">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt=""
+                />
+              </div>
+            </div>
+          <Footer/>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
